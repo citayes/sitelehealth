5690f17ebc67a0fc91231b374d425fece9a10db3
citayes <cita.indraswari@gmail.com>
2015-05-13 07:25:11 +0700

    nambah

--- application/models/jadwal_jaga.php
+++ application/models/jadwal_jaga.php
@@ -3,13 +3,31 @@
 		var $has_one = array('drg_ortodonti','admin');
 				var $validation = array(
     	'jam_mulai' => array(
-     	'rules' => array('required', 'numeric')
+     	'rules' => array('required', 'numeric', 'greater_than' => 1, 'less_than' =>24)
     	),
     	'jam_selesai' => array(
-     	'rules' => array('required', 'numeric')
+     	'rules' => array('required', 'numeric', 'greater_than' => 1, 'less_than' =>24)
     	),
     	'drg_ortodonti_id' => array(
      	'rules' => array('required')
     	)
     );
+
+    //     var $validation = array(
+    // 'Doctor' => array(
+    //     'label' => 'Doctor',
+    //     'rules' => array('required')
+    // ),
+    // 'Start' => array(
+    //     'label' => 'Start',
+    //     'rules' => array('required', 'numeric')
+    // ),
+    // 'End' => array(
+    //     'label' => 'End',
+    //     'rules' => array('required', 'numeric')
+    // )
+//);
+
 	}
+
+    
